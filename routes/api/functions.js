145b4e295b7e97bacbf6121a8fccc0c114f30a5a
Amith Raravi <amith.raravi@gmail.com>
2020-04-22 19:28:03 +0200

    Added HEROKU Server to RESETPASSWORD.

--- routes/api/functions.js
+++ routes/api/functions.js
@@ -174,7 +174,7 @@ const forgotPassword = (req, res) => {
             text:
               'You are receiving this because you (or someone else) have requested the reset of the password for your account.\n\n'
               + 'Please click on the following link, or paste this into your browser to complete the process within one hour of receiving it:\n\n'
-              + `http://localhost:8000/resetpassword\n\n`
+              + `https://mysterious-forest-21747.herokuapp.com/resetpassword\n\n`
               + `Reset Code: ${randomString}\n\n`
               + 'If you did not request this, please ignore this email and your password will remain unchanged.\n',
           };
